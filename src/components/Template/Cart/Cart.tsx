c109d1468fb5d63f478dd162625ce926365585da
nguyenduong1803 <duonghd1803@gmail.com>
2022-11-29 09:38:14 +0700

    feat: create cart, cart item

---
+++ src/components/Template/Cart/Cart.tsx
@@ -0,0 +1,25 @@
+import { Drawer, IconButton,Badge, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+type Props = {};
+
+const Cart = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <IconButton size="medium" sx={{ ml: 2 }} aria-haspopup="true">
+        <Badge badgeContent={4} color="secondary" onClick={()=>setOpen(true)}>
+          <ShoppingCartIcon fontSize="medium" />
+        </Badge>
+      </IconButton>
+      <Drawer open={open} anchor="right" onClose={() => setOpen(false)}>
+        <Box width="400px" >
+            <Typography textAlign="center" m={4} variant="h5">Shoping Cart</Typography>
+            
+        </Box>
+      </Drawer>
+    </>
+  );
+};
+export default Cart;
---
+++ src/components/Template/Cart/CartItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+type Props = {}
+
+const CartItem = (props: Props) => {
+  return (
+    <div>CartItem</div>
+  )
+}
+export default CartItem
